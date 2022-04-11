50b61171632d17f4efdd663cfc283003c9bac94f
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-11 20:23:17 +0300

    feat: finish nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/navbar/NavBar';
+import Home from './components/pages/home/Home';
+import About from './components/pages/about/About';
+import Portfolio from './components/pages/portfolio/Portfolio';
+import Contact from './components/pages/contact/Contact';
+import NoteFound from './components/pages/Notefound/NoteFound';
+
 
 import './app.scss';
-import NavBar from './components/navbar/NavBar';
+
 
 function App() {
   return (
     <div className="app">
-      <NavBar />
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='portfolio' element={<Portfolio />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='*' element={<NoteFound />} />
+        </Routes>
+      </main>
     </div>
   );
 }
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import burger from '../assets/icon/burger.svg'
 import './navbar.scss'
 
 
+
 function NavBar() {
+
+    const [activMenu, setActivMenu] = useState(false);
+
+    const showMenu = () => {
+        setActivMenu(!activMenu);
+    }
+
+    useEffect(() => {
+
+    })
+
     return (
-        <div>
-            <h1>NavBar</h1>
+        <div className=" header-nav">
+            <div className="container header-nav__container">
+                <div className="logo">
+                    <Link to='/'>logo</Link>
+                </div>
+                <div onClick={showMenu} className="burger">
+                    <img src={burger} alt="" />
+                </div>
+                <div className={activMenu ? "nav nav--show" : "nav "}>
+                    <NavLink onClick={showMenu} to='/'>Home</NavLink>
+                    <NavLink onClick={showMenu} to='/about'>About</NavLink>
+                    <NavLink onClick={showMenu} to='/portfolio'>Portfolio</NavLink>
+                    <NavLink onClick={showMenu} to='/contact'>Contact</NavLink>
+                </div>
+
+            </div>
         </div>
     )
 }
