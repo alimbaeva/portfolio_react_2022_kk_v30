62c36a86e4cf23a8696f2382faa77975ab4804a6
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-11 09:00:50 +0300

    init: initial app react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './app.scss';
+import NavBar from './components/navbar/NavBar';
+
+function App() {
+  return (
+    <div className="app">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navbar.scss'
+
+
+function NavBar() {
+    return (
+        <div>
+            <h1>NavBar</h1>
+        </div>
+    )
+}
+
+export default NavBar
