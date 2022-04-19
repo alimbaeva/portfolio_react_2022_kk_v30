a50739d1a34c140e69156eb5c763aba7a16fb49f
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-19 13:09:14 +0300

    feat: home page

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import burger from '../assets/icon/burger.svg'
 import './navbar.scss'
@@ -13,24 +13,52 @@ function NavBar() {
         setActivMenu(!activMenu);
     }
 
-    useEffect(() => {
-
-    })
+    const menuItems = [
+        {
+            id: 1,
+            menu: 'home',
+            link: '/'
+        },
+        {
+            id: 2,
+            menu: 'about',
+            link: 'about'
+        },
+        {
+            id: 3,
+            menu: 'portfolio',
+            link: 'portfolio'
+        },
+        {
+            id: 4,
+            menu: 'contact',
+            link: 'contact'
+        }
+    ]
 
     return (
         <div className=" header-nav">
             <div className="container header-nav__container">
                 <div className="logo">
-                    <Link to='/'>logo</Link>
+                    <Link to='/'>Asel</Link>
                 </div>
                 <div onClick={showMenu} className="burger">
                     <img src={burger} alt="" />
                 </div>
                 <div className={activMenu ? "nav nav--show" : "nav "}>
-                    <NavLink onClick={showMenu} to='/'>Home</NavLink>
+
+                    {menuItems.map((menu) => {
+                        return (
+                            <NavLink key={menu.id} onClick={showMenu} to={menu.link}>{menu.menu}</NavLink>
+                        )
+                    })
+
+                    }
+
+                    {/* <NavLink onClick={showMenu} to='/'>Home</NavLink>
                     <NavLink onClick={showMenu} to='/about'>About</NavLink>
                     <NavLink onClick={showMenu} to='/portfolio'>Portfolio</NavLink>
-                    <NavLink onClick={showMenu} to='/contact'>Contact</NavLink>
+                    <NavLink onClick={showMenu} to='/contact'>Contact</NavLink> */}
                 </div>
 
             </div>
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Photo from '../../assets/images/asel.jpg';
+import { SiCodewars, SiGithub, SiTelegram } from 'react-icons/si';
+import { init } from 'ityped'
+import './home.scss'
 
 function Home() {
+
+    const onimatateTextRef = useRef();
+    useEffect(() => {
+        init(onimatateTextRef.current, {
+            showCursor: true,
+            strings: ['Frontent React Developer'],
+            backDelay: 1500,
+            backSpeed: 60,
+        })
+    }, [])
+
     return (
-        <section>
-            <h1>Home pages</h1>
+        <section className='home'>
+            <div className="container home__container">
+                <div className="home__left">
+                    <h1>Asel Alimbaeva</h1>
+                    <p className='home__subtitle'><span ref={onimatateTextRef}></span></p>
+                    <p>
+                        A year ago, I was faced with a choice, to work at my job and devote less time to my children, or to leave my job and find another. But what?
+
+                        I started to study different professions, modern and actual professions, professions of the future. I thought a lot and thought about how I could make sure that all my needs were met. And I stopped in the field of IT. From the first, I began to study and write codes on my own to solve problems in Python,
+                        and I realized that I really like it. And seriously engaged in the transformation of his profession.
+                    </p>
+                    <ul>
+                        <li><a href="https://t.me/aselAl86" target="_blank" rel='noopener noreferrer'>
+                            <SiTelegram className='home__icon' />
+                        </a></li>
+                        <li><a href="https://github.com/alimbaeva" target="_blank" rel='noopener noreferrer'>
+                            <SiGithub className='home__icon' />
+                        </a></li>
+                        <li><a href="https://www.codewars.com/users/alimbaeva" target="_blank" rel='noopener noreferrer'>
+                            <SiCodewars className='home__icon' />
+                        </a></li>
+                    </ul>
+                    <Link to='/contact' className='home__btn'>Hire Me</Link>
+                </div>
+                <div className="home__right">
+                    <img src={Photo} alt="Hero foto" />
+                </div>
+            </div>
         </section>
     )
 }
